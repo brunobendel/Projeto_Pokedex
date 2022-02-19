3a8afeaa6794d90e4d7f738dee51eb819644ba1f
brunobendel <75317932+brunobendel@users.noreply.github.com>
2022-02-19 00:01:29 -0300

    Pokedex

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface pokedexProps {
+
 }
 
-export default App;
+const pokedex: React.FC = () => {
+
+  const [pokemons, setPokemons] = useState<string[]>([]);
+  const [selectedPokemon, setSelectedPokemon] = useState<string | undefined>('PIKACHU');
+
+  return (
+    <div>
+      <h1>Pokedex</h1>
+
+      <h2>Pokemon Selecionado: {selectedPokemon} </h2>
+    </div>
+  );
+
+}
+
+export default pokedex;
---
+++ src/pokedex/pokedex.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface pokedexProps {
+
+}
+
+export const pokedex: React.FC<pokedexProps> = () => {
+    return (
+        <div>
+            pokedex
+        </div>
+    );
+};
+
+export default pokedex;
